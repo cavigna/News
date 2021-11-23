d198c1a6ed8b763b2ecad925a8e65cb7461d4cd4
cavigna <40524472+cavigna@users.noreply.github.com>
2021-11-23 13:09:44 -0300

    Functional App

--- app/src/main/java/com/example/news/ui/FavFragment.kt
+++ app/src/main/java/com/example/news/ui/FavFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.news.R
@@ -55,6 +56,7 @@ class FavFragment : Fragment(), FavListAdapter.MiBorradorDeNoticias {
 
     override fun borrar(favorito: NewsFavEntity) {
         viewModel.eliminarFavorito(favorito)
+        Toast.makeText(requireContext(), "Noticia eliminada", Toast.LENGTH_SHORT).show()
     }
 
 
